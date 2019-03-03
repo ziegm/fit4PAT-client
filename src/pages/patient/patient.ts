584425ea3984e0dc35c0759935d9f2c681ce6f76
Michaela Ziegler <michaela.ziegler@students.bfh.ch>
2019-03-03 18:49:20 +0100

    feature#2: Added new tabs.

---
+++ src/pages/patient/patient.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  selector: 'page-patient',
+  templateUrl: 'patient.html'
+})
+export class PatientPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
