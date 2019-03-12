156fd41071c506dd219cddbfcffe39156291e9d3
Mirjam Kaufmann <kaufm5@bfh.ch>
2019-03-12 14:31:37 +0100

    Aufbau Timed Walking Test und DEMMI

--- src/pages/assessments/assessment-forms/form-walkingtest/form-walkingtest.ts
+++ src/pages/assessments/assessment-forms/form-walkingtest/form-walkingtest.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import {App, IonicPage, NavController, NavParams} from 'ionic-angular';
 import {EvaluationWalkingtestPage} from "../../assessment-evaluations/evaluation-walkingtest/evaluation-walkingtest";
 
@@ -15,6 +16,13 @@ import {EvaluationWalkingtestPage} from "../../assessment-evaluations/evaluation
   templateUrl: 'form-walkingtest.html',
 })
 export class FormWalkingtestPage {
+  public timeBegan = null
+  public timeStopped:any = null
+  public stoppedDuration:any = 0
+  public started = null
+  public running = false
+  public blankTime = "00:00.000"
+  public time = "00:00.000"
 
   private rootNav:any;
 
@@ -29,5 +37,57 @@ export class FormWalkingtestPage {
   navToEvaluationWalkingtest(){
     this.rootNav.push(EvaluationWalkingtestPage);
   }
+
+  timeInText(){
+    var inputOne;
+    inputOne = this.time + " ";
+  }
+
+  start() {
+    if(this.running) return;
+    if (this.timeBegan === null) {
+      this.reset();
+      this.timeBegan = new Date();
+    }
+    if (this.timeStopped !== null) {
+      let newStoppedDuration:any = (+new Date() - this.timeStopped)
+      this.stoppedDuration = this.stoppedDuration + newStoppedDuration;
+    }
+    this.started = setInterval(this.clockRunning.bind(this), 10);
+    this.running = true;
+  }
+  stop() {
+    this.running = false;
+    this.timeStopped = new Date();
+    clearInterval(this.started);
+  }
+  reset() {
+    this.running = false;
+    clearInterval(this.started);
+    this.stoppedDuration = 0;
+    this.timeBegan = null;
+    this.timeStopped = null;
+    this.time = this.blankTime;
+  }
+  zeroPrefix(num, digit) {
+    let zero = '';
+    for(let i = 0; i < digit; i++) {
+      zero += '0';
+    }
+    return (zero + num).slice(-digit);
+  }
+  clockRunning(){
+    let currentTime:any = new Date()
+    let timeElapsed:any = new Date(currentTime - this.timeBegan - this.stoppedDuration)
+    let hour = timeElapsed.getUTCHours()
+    let min = timeElapsed.getUTCMinutes()
+    let sec = timeElapsed.getUTCSeconds()
+    let ms = timeElapsed.getUTCMilliseconds();
+    this.time =
+      this.zeroPrefix(hour, 2) + ":" +
+      this.zeroPrefix(min, 2) + ":" +
+      this.zeroPrefix(sec, 2) + "." +
+      this.zeroPrefix(ms, 3);
+  };
 
 }
